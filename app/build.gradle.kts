d9fc0d2d80c44e52c95c5f774391a9a2eeda0e60
vitajofita@gmail.com <vitajofita@gmail.com>
2024-11-19 21:37:58 +0800

    Halaman Home 50%

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation (libs.androidx.cardview)
+    implementation (libs.material.v1100)
 }
