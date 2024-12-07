0ee1d21d891c4d3d3e275e5f404654685cd932fe
najwa aulia <safinatunnajahnajwa@gmail.com>
2024-12-07 13:12:06 +0700

    detail 50%

--- app/src/main/java/com/capstone/kulinerkita/ui/search/SearchActivity.kt
+++ app/src/main/java/com/capstone/kulinerkita/ui/search/SearchActivity.kt
@@ -1,12 +1,15 @@
 package com.capstone.kulinerkita.ui.search
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.capstone.kulinerkita.data.model.Restaurant
 import com.capstone.kulinerkita.databinding.ActivitySearchBinding
+import com.capstone.kulinerkita.ui.detailResto.DetailRestoActivity
 import com.capstone.kulinerkita.ui.home.HomeAdapter
 
 @Suppress("DEPRECATION")
@@ -22,15 +25,14 @@ class SearchActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // Dapatkan data restoran dari Intent
-        restaurantList = intent.getParcelableArrayListExtra("restaurantList") ?: listOf()
+        restaurantList = intent.getParcelableArrayListExtra("RESTAURANT_LIST") ?: listOf()
 
         // Setup RecyclerView
-        restaurantAdapter = HomeAdapter(restaurantList.toMutableList()) {
-            // Handle item click jika diperlukan
-            // Misalnya: Buka DetailActivity
-            // startActivity(Intent(this, DetailActivity::class.java).apply {
-            //     putExtra("restaurant", restaurant)
-            // })
+        restaurantAdapter = HomeAdapter(restaurantList.toMutableList()) { selectedRestaurant ->
+            // Handle item klik, misalnya navigasi ke DetailRestoActivity
+            startActivity(Intent(this, DetailRestoActivity::class.java).apply {
+                 putExtra("SELECTED_RESTAURANT_ID", selectedRestaurant.id)
+             })
         }
 
         binding.rvSearchResults.apply {
@@ -50,8 +52,15 @@ class SearchActivity : AppCompatActivity() {
 
     private fun filterRestaurant(query: String) {
         val filteredList = restaurantList.filter {
-            it.name.contains(query, ignoreCase = true)
+            it.name.contains(query, ignoreCase = true) || it.address.contains(query, ignoreCase = true)
         }
         restaurantAdapter.updateData(filteredList)
+
+        // Tampilkan pesan jika hasil pencarian kosong
+        if (filteredList.isEmpty()) {
+            binding.tvNoResults.visibility = View.VISIBLE
+        } else {
+            binding.tvNoResults.visibility = View.GONE
+        }
     }
 }
